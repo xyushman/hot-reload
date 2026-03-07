d64b1f18623e9b43205277641abbe14a9eed16f1
KafeOvid <ayushmanng04@gmail.com>
2026-03-07 19:19:24 +0530

    add sample testserver for demonstration

---
+++ testserver/helpers/message.go
@@ -0,0 +1,5 @@
+package helpers
+
+func GetMessage() string {
+	return "This message was generated from the helpers package!"
+}
---
+++ testserver/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"hotreload/testserver/helpers"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("testserver/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		data := struct {
+			PID      int
+			Time     string
+			ExtraMsg string
+		}{
+			PID:      os.Getpid(),
+			Time:     time.Now().Format(time.RFC1123),
+			ExtraMsg: helpers.GetMessage(),
+		}
+
+		t.Execute(w, data)
+	})
+	
+	log.Printf("Server starting on :%s (pid %d)", port, os.Getpid())
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
